a090ab82b48b7bc178426ffa084bfd58044365dd
yefeng100 <chxf4630@gmail.com>
2025-01-03 14:53:37 +0700

    fix

--- contracts/coct/coctNftFactory.sol
+++ contracts/coct/coctNftFactory.sol
@@ -15,12 +15,33 @@ contract CoctNftFactory{
         owner = msg.sender;
     }
 
+    //自己地址
+    function getAddress() public view returns (address) {
+        return address(this);
+    }
+
+    //获取owner地址的余额
+    function getOwnerBalance() public view returns (uint) {
+        uint ownerBalance = owner.balance;
+        return ownerBalance;
+    }
+
     //部署nft
     function deployNft(string memory _name, string memory _symbol, string memory _uri, uint _price, address _payAddress) public {
         require(msg.sender == owner, "must be on owner");
         address nftAddress = address(new CoctNft(_name, _symbol, _uri, _price, _payAddress));
         nftContractsMap[nftAddress] = _name;
         nftContracts.push(nftAddress);
+    }
+
+    //获取nft地址
+    function getNftAddressList() public view returns (address[] memory){
+        return nftContracts;
+    }
+
+    //获取name
+    function getNftName(address _nftAddress) public view returns (string memory){
+        return CoctNft(_nftAddress).getName();
     }
 
     //设置合约价格
